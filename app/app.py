8a5af0d5cdaa1fe6523f88eaec1d2e0577f25f94
Arica Jackson <aricajackson2020@gmail.com>
2020-11-02 20:34:01 -0500

    update scrape_mars file

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 #flask app
 #serve up a jumbotron with a button in it
-#first class activity on scraping {{  }}
+#first class activity on scraping 
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+
 import scrape_mars
 
 app = Flask(__name__)
@@ -25,18 +26,11 @@ def scrape():
     #create mars collection in mongodb mars db
     mars = mongo.db.mars
 
-    #call the function scrape all and assign to mars data
+    #call the function scrape all and assign to mars data(used in scrape app have to complete first)
     mars_data = scrape_mars.scrape_all_content()
     mars.update({}, mars_data, upsert=True)
 
     return "Scraping Complete - Please return to homepage and refresh to view scraped Mars news, facts and images, thank you"
-    
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
--- app/scrape_mars.py
+++ app/scrape_mars.py
@@ -1,1 +1,37 @@
- #my scrapping app
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# import datetime as dt
+ 
+
+def scrape_all_content():
+     #initialize brower - chrome (in resources)
+    browser = Browser("chrome", executable_path="/resources/chromedriver.exe", headless=True)
+
+    news_title, news_para = mars_news(browser)
+
+     #run all scraping functions and store in dictionary
+
+    mars_data= {
+        "news_title": news_title,
+    }
+
+    browser.quit()
+    return mars_data
+
+def mars_news(browser):
+    #scrape for mars news url
+    url = "https://mars.nasa.gov/news"
+    browser.visit(url)
+
+    html = browser.html
+    mars_news_soup = BeautifulSoup(html, "html.parser")
+
+    
+    # same as selecting bs; call (select_one)
+    slide_element = mars_news_soup.select_one("ul.item_list li.slide")
+
+    news_title = slide_element.find("div", class_="content_title").get_text()
+      
+    return news_title
