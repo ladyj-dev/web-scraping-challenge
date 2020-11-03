e9728471fd2befe5ce5e2882cf854fce88deb9dc
Arica Jackson <aricajackson2020@gmail.com>
2020-11-02 19:51:13 -0500

    scraping

--- app/app.py
+++ app/app.py
@@ -16,9 +16,27 @@ mongo = PyMongo(app)
 #create home route
 @app.route("/")
 def index():
+    #identify the name of the db collection mars
     mars = mongo.db.mars.find_one()
-
     return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    #create mars collection in mongodb mars db
+    mars = mongo.db.mars
+
+    #call the function scrape all and assign to mars data
+    mars_data = scrape_mars.scrape_all_content()
+    mars.update({}, mars_data, upsert=True)
+
+    return "Scraping Complete - Please return to homepage and refresh to view scraped Mars news, facts and images, thank you"
+    
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
